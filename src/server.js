3cf0203dc8fe5aa2a73d49c041eed244e556e1ec
大溝桂 <kay@KeiOmizo-MacBook-Pro.local>
2021-08-31 17:28:39 +0900

    create

---
+++ src/server.js
@@ -0,0 +1,39 @@
+var http = require('http')
+var os = require('os')
+var url = require('url')
+
+const timer = require('timers/promises')
+
+http.createServer(function (req, res) {
+
+    var PREF="[NODE_JS_APP] ";
+
+    var query = url.parse(req.url, true).query;
+    var msg = "DUMMY"
+
+    if (query['msg']) {
+        msg = query['msg']
+    }
+      
+    console.log("log: " + PREF + msg)
+    console.error("error: " + PREF + msg)
+    console.info("info: " + PREF + msg)
+    console.trace("trace: " + PREF + msg)
+
+    zzz();
+
+
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    const host = os.hostname()
+    res.end(`Hello World ${host} \n`)
+}).listen(8080)
+
+
+
+async function zzz() {
+   console.log("before");
+   const res = await timer.setTimeout(20000, 'result');
+   console.log(res);
+   console.log("shutdown");
+   process.exit(1);
+}
